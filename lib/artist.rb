dfa4a35a651394d88966bc2037f1d40f451400d1
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-03 00:09:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,12 +4,22 @@ class Artist
   
   def initialize(name)
     @name = name
-    
-  
+    @@all << self
+  end
   
   def self.all
     @@all 
   end
+  
+  def new_song(name, genre)
+    new_song = Song.new(name, genre)
+    new_song.artist = self
+  end
+  
+  def songs
+    Song.all.select do |song|
+      
+    
   
   
 end
